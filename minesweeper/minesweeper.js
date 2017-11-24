53bc18495ea835831b083033ea990ee62bfa99d4
laxmi <eisu.it.student05@gmail.com>
2017-11-24 03:22:58 +0000

    creat 25 box

---
+++ minesweeper/minesweeper.js
@@ -0,0 +1,108 @@
+'use strict';
+
+$(function() {
+ var $divminesweeperPos = $("#minesweeper");
+
+
+ var $divBtn1 = $("<div>", { "id": "btn1", "class": "btn1" });
+ $divBtn1.text();
+ $divminesweeperPos.append($divBtn1);
+ 
+ var $divBtn2 = $("<div>", { "id": "btn2", "class": "btn2" });
+ $divBtn2.text();
+ $divminesweeperPos.append($divBtn2);
+ 
+ var $divBtn3 = $("<div>", { "id": "btn3", "class": "btn3" });
+ $divBtn3.text();
+ $divminesweeperPos.append($divBtn3);
+ 
+ var $divBtn4 = $("<div>", { "id": "btn4", "class": "btn4" });
+ $divBtn4.text();
+ $divminesweeperPos.append($divBtn4);
+ 
+ var $divBtn5 = $("<div>", { "id": "btn5", "class": "btn5" });
+ $divBtn5.text();
+ $divminesweeperPos.append($divBtn5);
+ 
+ var $divBtn6 = $("<div>", { "id": "btn6", "class": "btn6" });
+ $divBtn6.text();
+ $divminesweeperPos.append($divBtn6);
+ 
+ var $divBtn7 = $("<div>", { "id": "btn7", "class": "btn7" });
+ $divBtn7.text();
+ $divminesweeperPos.append($divBtn7);
+ 
+ var $divBtn8 = $("<div>", { "id": "btn8", "class": "btn8" });
+ $divBtn8.text();
+ $divminesweeperPos.append($divBtn8);
+ 
+ var $divBtn9 = $("<div>", { "id": "btn9", "class": "btn9" });
+ $divBtn9.text();
+ $divminesweeperPos.append($divBtn9);
+ 
+ var $divBtn10 = $("<div>", { "id": "btn10", "class": "btn10" });
+ $divBtn10.text();
+ $divminesweeperPos.append($divBtn10);
+ 
+ var $divBtn11 = $("<div>", { "id": "btn11", "class": "btn11" });
+ $divBtn11.text();
+ $divminesweeperPos.append($divBtn11);
+ 
+  var $divBtn12 = $("<div>", { "id": "btn12", "class": "btn12" });
+ $divBtn12.text();
+ $divminesweeperPos.append($divBtn12);
+ 
+ var $divBtn13 = $("<div>", { "id": "btn13", "class": "btn13" });
+ $divBtn13.text();
+ $divminesweeperPos.append($divBtn13);
+ 
+ var $divBtn14 = $("<div>", { "id": "btn14", "class": "btn14" });
+ $divBtn14.text();
+ $divminesweeperPos.append($divBtn14);
+ 
+ var $divBtn15 = $("<div>", { "id": "btn15", "class": "btn15" });
+ $divBtn15.text();
+ $divminesweeperPos.append($divBtn15);
+ 
+ var $divBtn16 = $("<div>", { "id": "btn16", "class": "btn16" });
+ $divBtn16.text();
+ $divminesweeperPos.append($divBtn16);
+ 
+ var $divBtn17 = $("<div>", { "id": "btn17", "class": "btn17" });
+ $divBtn17.text();
+ $divminesweeperPos.append($divBtn17);
+ 
+ var $divBtn18 = $("<div>", { "id": "btn18", "class": "btn18" });
+ $divBtn17.text();
+ $divminesweeperPos.append($divBtn18);
+ 
+ var $divBtn19 = $("<div>", { "id": "btn19", "class": "btn19" });
+ $divBtn19.text();
+ $divminesweeperPos.append($divBtn19);
+ 
+ var $divBtn20 = $("<div>", { "id": "btn20", "class": "btn20" });
+ $divBtn20.text();
+ $divminesweeperPos.append($divBtn20);
+ 
+ var $divBtn21 = $("<div>", { "id": "btn21", "class": "btn21" });
+ $divBtn21.text();
+ $divminesweeperPos.append($divBtn21);
+
+ var $divBtn22 = $("<div>", { "id": "btn22", "class": "btn22" });
+ $divBtn22.text();
+ $divminesweeperPos.append($divBtn22);
+ 
+ var $divBtn23 = $("<div>", { "id": "btn23", "class": "btn23" });
+ $divBtn23.text();
+ $divminesweeperPos.append($divBtn23);
+ 
+ var $divBtn24 = $("<div>", { "id": "btn24", "class": "btn24" });
+ $divBtn24.text();
+ $divminesweeperPos.append($divBtn24);
+ 
+ var $divBtn25 = $("<div>", { "id": "btn25", "class": "btn25" });
+ $divBtn25.text();
+ $divminesweeperPos.append($divBtn25);
+
+
+});
